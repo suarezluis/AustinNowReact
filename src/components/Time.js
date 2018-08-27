689cf77c85abd0be0546acfce97c22c864408738
Luis Suarez <suarezluis@gmail.com>
2018-08-26 20:41:41 -0500

    created live time display functionality

--- src/components/Time.js
+++ src/components/Time.js
@@ -2,8 +2,28 @@ import React, { Component } from "react";
 import { getTime } from "../timeHelpers";
 
 class Time extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      format: 12,
+      time: getTime(12)
+    };
+    this.interval = setInterval(() => {
+      this.setState({ time: getTime(12) });
+    }, 1000);
+  }
   render() {
-    return <div>{getTime(12)}</div>;
+    return <div>{this.state.time}</div>;
+  }
+
+  // setting up interval for live time display
+  componentDidMount() {
+    this.interval;
+  }
+
+  //clear the interval before unmounting
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 }
 
